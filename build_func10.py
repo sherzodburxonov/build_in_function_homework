6201204c9ff4a9ec0559912952d77b4775e49c52
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-04-11 09:23:53 +0500

    10

--- build_func10.py
+++ build_func10.py
@@ -1,4 +1,6 @@
+from math import pow
 def main(x, y):
+    
     """Integer type variables 'x' and 'y' are given. Return the value of the expression in README.md file.
     https://github.com/codeschool43/Build_in_function_homework#build_func10
 
@@ -9,5 +11,5 @@ def main(x, y):
     Returns:
         int: the value of the expression
     """
-    return 3*y**(1/2)+x**(2/3)
+    return 3*pow(y,(1/2))+pow(x,(2/3))
 print(main(8,4))
